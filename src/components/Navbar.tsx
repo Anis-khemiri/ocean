f97eaadb6035f41a1eb8c7d707ccc39e8b339c3c
Anis-khemiri <aniskhemiri6@gmail.com>
2024-01-09 18:04:58 +0100

    add sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,27 @@
+"use client"
+
 import Link from "next/link";
 import Logo from "./Logo";
 import { navLinks } from "@/constants/nav-links";
 import MobileNav from "./MobileNav";
+import { useEffect, useRef } from "react";
 
 export default function Navbar() {
+  const headerRef:any = useRef();
+  useEffect(()=>{
+window.addEventListener("scroll", ()=>{
+if(window.scrollY > 100){
+  headerRef.current.style.background ="#0c1524";
+  headerRef.current.style.padding ="10px 0";
+}else{
+  headerRef.current.style.background ="";
+  headerRef.current.style.padding = "";
+
+}
+})
+  }, []);
   return (
-    <header className=" fixed top-0 left-0 w-full bg-slate-900/50 backdrop-blur-md z-30">
+    <header  ref={headerRef} className=" fixed top-0 left-0 w-full bg-slate-900/50 backdrop-blur-md z-30 transition-all duration-200">
       <div className="relative container py-6 flex justify-between transition-all">
         <Logo />
 
